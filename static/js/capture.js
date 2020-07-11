ca2632e10e2eec426d3fb037e643e12a38c4646f
Andrii Yurkiv <andrii.yurkiv2@globallogic.com>
2020-07-11 13:34:50 +0300

    version 1.4: feformat UI

--- static/js/capture.js
+++ static/js/capture.js
@@ -1,27 +1,5 @@
 
 
-function addToLocalStorage() {
-
-    canvas = document.getElementById('canvas');
-
-    img_input = document.getElementById('file_input');
-    var file = img_input.files[0];
-
-    var context = canvas.getContext('2d');
-
-    var img = new Image;
-
-    img.onload = function () {
-        context.drawImage(img, 0, 0);
-    }
-    img.src = URL.createObjectURL(file);
-    var dataURL = canvas.toDataURL("image/png");
-    localStorage.setItem('submission', dataURL);
-}
-
-
-
-
 (function() {
 
     var width = 640;
@@ -32,55 +10,71 @@ function addToLocalStorage() {
     var video = null;
     var canvas = null;
     var photo = null;
-    var startbutton = null;
+    var estimate_button = null;
 
     var download_image = null;
     var download_json = null;
-    var download_coco = null;
 
     var upload_image = null;
     var submit_url = null;
 
     function startup() {
-        video = document.getElementById('video');
+
         canvas = document.getElementById('canvas');
         photo = document.getElementById('photo');
-        startbutton = document.getElementById('startbutton');
+        estimate_button = document.getElementById('estimate_button');
 
         download_image = document.getElementById('download_image');
-        download_coco = document.getElementById('download_coco');
         download_json = document.getElementById('download_json');
 
-//        upload_image = document.getElementById('file_submit');
-//        submit_url = document.getElementById('link_submit');
+        if (document.getElementById('webcamChoice').checked) {
 
-        navigator.mediaDevices.getUserMedia({video: true, audio: false})
-        .then(function(stream){
-            video.srcObject = stream;
-            video.play();
-        })
-        .catch(function(err) {
-            console.log("An error occurred: " + err)
-        });
+            document.getElementById('uploaded_image').style.display = 'none';
+            document.getElementById('url_image').style.display = 'none';
 
-        video.addEventListener('canplay', function(ev) {
-            if (!streaming) {
-                height = video.videoHeight / (video.videoWidth / width);
+            document.getElementById('file_form').style.display = 'none';
+            document.getElementById('link_form').style.display = 'none';
 
-                if (isNaN(height)) {
-                    height = width / (4/3);
+            video = document.getElementById('video');
+
+            navigator.mediaDevices.getUserMedia({video: true, audio: false})
+            .then(function(stream){
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(function(err) {
+                console.log("An error occurred: " + err)
+            });
+
+            video.addEventListener('canplay', function(ev) {
+                if (!streaming) {
+                    height = video.videoHeight / (video.videoWidth / width);
+
+                    if (isNaN(height)) {
+                        height = width / (4/3);
+                    }
+
+                    video.setAttribute('width', width);
+                    video.setAttribute('height', height);
+                    canvas.setAttribute('width', width);
+                    canvas.setAttribute('height', height);
+                    streaming = true;
                 }
+            }, false);
 
-                video.setAttribute('width', width);
-                video.setAttribute('height', height);
-                canvas.setAttribute('width', width);
-                canvas.setAttribute('height', height);
-                streaming = true;
-            }
-        }, false);
+        } else if (document.getElementById('imageChoice').checked) {
+            document.getElementById('video').style.display = 'none';
+            document.getElementById('link_form').style.display = 'none';
+            document.getElementById('url_image').style.display = 'none';
+
+        } else if (document.getElementById('urlChoice').checked) {
+            document.getElementById('uploaded_image').style.display = 'none';
+            document.getElementById('file_form').style.display = 'none';
+            document.getElementById('video').style.display = 'none';
+        }
 
 
-        startbutton.addEventListener('click', function(ev){
+        estimate_button.addEventListener('click', function(ev){
             processImage();
             ev.preventDefault();
         }, false);
@@ -120,7 +114,7 @@ function addToLocalStorage() {
 
     function emptyImage() {
         var context = canvas.getContext('2d');
-        context.fillStyle = "#AAA";
+        context.fillStyle = "#000";
         context.fillRect(0, 0, canvas.width, canvas.height);
 
         var data = canvas.toDataURL('image/png');
@@ -128,20 +122,40 @@ function addToLocalStorage() {
     }
 
     async function processImage() {
+
         var context = canvas.getContext('2d');
+        canvas.width = width;
+        canvas.height = height;
 
-        if (streaming) {
-          canvas.width = width;
-          canvas.height = height;
+        if (document.getElementById('webcamChoice').checked) {
+            if (streaming) {
+                context.drawImage(video, 0, 0, width, height);
+            } else {
+                emptyImage();
+            }
+        } else if (document.getElementById('imageChoice').checked) {
+            image_input = document.getElementById('file_input');
+            img = document.getElementById('uploaded_image');
 
-          context.drawImage(video, 0, 0, width, height);
-          localStorage.setItem('current_canvas', canvas.toDataURL('image/png'));
+            if (image_input.files[0]) {
+                img.onload = function() {
+                    context.drawImage(img, 0, 0, width, height);
+                }
+                img.src = URL.createObjectURL(image_input.files[0]);
+            } else {
+                emptyImage();
+            }
+        } else if (document.getElementById('urlChoice').checked) {
 
-          let imageBlob = await new Promise(resolve => canvas.toBlob(resolve, 'image/png'));
+        }
+        console.log(canvas);
 
-          prediction = makePrediction(imageBlob);
+        localStorage.setItem('current_canvas', canvas.toDataURL('image/png'));
+        let imageBlob = await new Promise(resolve => canvas.toBlob(resolve, 'image/png'));
 
-          prediction.then(function(result) {
+        prediction = makePrediction(imageBlob);
+
+        prediction.then(function(result) {
 
             for (var face_id in result) {
                 bbox = result[face_id]['bbox']
@@ -159,65 +173,13 @@ function addToLocalStorage() {
                 context.fillText(category + ': ' + score.toFixed(2).toString(), bbox[0], bbox[1] - 15);
             }
 
-            var file = canvas.toDataURL('image/png');
-            photo.setAttribute('src', file);
-            localStorage.setItem('current_prediction', JSON.stringify(result))
+        var file = canvas.toDataURL('image/png');
+        photo.setAttribute('src', file);
+        localStorage.setItem('current_prediction', JSON.stringify(result))
 
-          }, function(error) {
+        }, function(error) {
             console.error("Error making prediction " + error);
-          });
-
-        } else if (localStorage.hasOwnProperty('submission')) {
-
-           var img = new Image;
-           dataImg = localStorage.getItem('submission');
-
-           console.log(dataImg);
-
-           canvas.width = width;
-           canvas.height = height;
-
-           img.onload = function () {
-                context.drawImage(img, 0, 0, width, height);
-           }
-           img.src = dataImg;
-
-           localStorage.setItem('current_canvas', canvas.toDataURL('image/png'));
-
-           console.log(img);
-
-           let imageBlob = await new Promise(resolve => canvas.toBlob(resolve, 'image/png'));
-
-           prediction = makePrediction(imageBlob);
-
-           prediction.then(function(result) {
-
-             for (var face_id in result) {
-                bbox = result[face_id]['bbox']
-                score = result[face_id]['score']
-                category = result[face_id]['category']
-
-                context.beginPath();
-                context.lineWidth = "4";
-                context.strokeStyle = "red";
-                context.rect(bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1]);
-                context.stroke();
-
-                context.font = "30px Ubuntu Mono";
-                context.fillStyle = "red";
-                context.fillText(category + ': ' + score.toFixed(2).toString(), bbox[0], bbox[1] - 15);
-            }
-
-           var file = canvas.toDataURL('image/png');
-           photo.setAttribute('src', file);
-           localStorage.setItem('current_prediction', JSON.stringify(result));
-           }, function(error) {
-            console.error("Error making prediction " + error);
-          });
-
-        } else {
-            emptyImage();
-        }
+        });
     }
 
     async function makePrediction(imageBlob) {
